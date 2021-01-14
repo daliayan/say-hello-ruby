a0ef0552b9016ed26ccacb7195af11e727a51d0a
daliayan <daliayan5@gmail.com>
2021-01-13 21:23:38 -0500

    'Say_hello'

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,14 @@
 # Build your say_hello method here
+
+def say_hello(name = "Coder")
+    puts "Hello, #{name}!"
+end
+
+def greeting(saying = "Jumbo")
+    puts "#{saying} to you my friend!"
+end
+
+puts greeting
+
+puts say_hello
+
